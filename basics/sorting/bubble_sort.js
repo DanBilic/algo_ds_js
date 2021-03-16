653325d8cb83e9bf9ad72ec38aede20300eeb8cb
DanBilic <bilic90@gmx.de>
2021-03-16 09:39:54 +0100

    sorting: bubble sort implemented

---
+++ basics/sorting/bubble_sort.js
@@ -0,0 +1,15 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+};
+
+const nums = [1, 5, 6, 3, 2, 9, 0];
+console.log(bubbleSort(nums));
