5052db4410396e32b6e61b2bb8dce6265c74e1ce
DanBilic <bilic90@gmx.de>
2021-03-18 20:48:13 +0100

    graphs: init

---
+++ basics/graphs/custom_graphs.js
@@ -0,0 +1,18 @@
+// edge list
+const graph = [
+  [0, 2],
+  [2, 3],
+  [2, 1],
+  [1, 3],
+];
+
+// adjacent list
+const graph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// adjacent matrix
+const garph = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0],
+];
