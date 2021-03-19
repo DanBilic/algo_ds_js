d0bacebf4604761bd7974e14d378aaa530140ec7
DanBilic <bilic90@gmx.de>
2021-03-19 10:47:12 +0100

    recursion: fibonacci implemented

---
+++ basics/recursion/fibonacci.js
@@ -0,0 +1,23 @@
+const fibonacciRecursive = (num) => {
+  if (num === 1) {
+    return 1;
+  }
+  if (num === 0) {
+    return 0;
+  }
+
+  return fibonacciRecursive(num - 2) + fibonacciRecursive(num - 1);
+};
+
+const fibonacciIterative = (num) => {
+  let arr = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+
+  return arr[num];
+};
+
+console.log("recursive: ", fibonacciRecursive(3));
+console.log("iterative: ", fibonacciIterative(3));
