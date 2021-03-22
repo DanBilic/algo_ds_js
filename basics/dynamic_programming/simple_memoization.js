4e868e457e48695db4f0c44cadcb4afb2cd80031
DanBilic <bilic90@gmx.de>
2021-03-22 19:19:00 +0100

    memoized: simple memoization implemented with closure

--- basics/dynamic_programming/simple_memoization.js
+++ basics/dynamic_programming/simple_memoization.js
@@ -8,3 +8,22 @@ const memoizedAddTo80 = (n) => {
     return cache[n];
   }
 };
+
+const memoizedAddTo80Closure = () => {
+  let cache = {};
+  return function (n) {
+    if (n in cache) {
+      console.log("from cache");
+      return cache[n];
+    } else {
+      console.log("not from cache");
+      cache[n] = n + 80;
+      return cache[n];
+    }
+  };
+};
+
+const memoized = memoizedAddTo80Closure();
+
+console.log(memoized(5));
+console.log(memoized(5));
