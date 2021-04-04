4dc60c4992dc722ed38d894ffcea40e18d4b232c
DanBilic <bilic90@gmx.de>
2021-04-04 19:56:05 +0200

    linked list: cycle detection

---
+++ practise/linkedList/(medium)cycle_detection.js
@@ -0,0 +1,46 @@
+const SinglyLinkedListNode = class {
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
+};
+
+const SinglyLinkedList = class {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertNode(nodeData) {
+    const node = new SinglyLinkedListNode(nodeData);
+
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+
+    this.tail = node;
+  }
+};
+
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+let llist = new SinglyLinkedList();
+
+for (let i = 0; i < llistCount; i++) {
+  llist.insertNode(llistItem);
+}
