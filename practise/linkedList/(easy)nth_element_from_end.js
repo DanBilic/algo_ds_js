da3c76566b159a095b1b72615b0115f4579d3fff
DanBilic <bilic90@gmx.de>
2021-04-04 19:20:32 +0200

    linked list: nth element from end solution O(1) space

--- practise/linkedList/(easy)nth_element_from_end.js
+++ practise/linkedList/(easy)nth_element_from_end.js
@@ -42,6 +42,27 @@ function getNode(head, positionFromTail) {
   return result;
 }
 
+// O(1) space solution
+// RUNNING pointer technique
+function getNodeRunningPointer(head, positionFromTail) {
+  // O(n) time, O(1) space complexity
+
+  let runner = head;
+  let result = head;
+
+  //move runner positionFromTail into the list
+  for (let i = 0; i <= positionFromTail; i++) {
+    runner = runner.next;
+  }
+
+  while (runner !== null) {
+    runner = runner.next;
+    result = result.next;
+  }
+
+  return result.data;
+}
+
 let llist = new SinglyLinkedList();
 
 for (let i = 0; i < llistCount; i++) {
