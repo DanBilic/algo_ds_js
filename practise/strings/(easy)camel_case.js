3fca5c2e740bda85e176ee0b2c95cb0205f17279
DanBilic <bilic90@gmx.de>
2021-04-06 10:46:13 +0200

    strings: camel case implemented

---
+++ practise/strings/(easy)camel_case.js
@@ -0,0 +1,11 @@
+// Complete the camelcase function below.
+function camelcase(s) {
+  let count = 1;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      count++;
+    }
+  }
+
+  return count;
+}
