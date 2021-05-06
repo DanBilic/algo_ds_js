01000f71a8c6c726e5b59610b193ba3ceb8a6f36
DanBilic <bilic90@gmx.de>
2021-05-06 20:04:21 +0200

    basic tree insertChild added

---
+++ basics/trees/basic_tree/tree.js
@@ -0,0 +1,14 @@
+class Tree {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+
+  insertChild(value) {
+    const newTree = new Tree(value);
+    this.children.push(newTree);
+    return newTree;
+  }
+
+  removeChild(value) {}
+}
