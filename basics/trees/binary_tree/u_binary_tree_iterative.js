6f9df9e9582c6a2da37c1f05e53168af4f34d20c
DanBilic <bilic90@gmx.de>
2021-05-07 12:01:28 +0200

    binary search tree: insert iterative solution added

---
+++ basics/trees/binary_tree/u_binary_tree_iterative.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverse(func = console.log) {
+    let current = this.root;
+    func(current);
+
+    while (current !== null) {
+      if (current.left) {
+        func(current.left);
+        current.left;
+      }
+    }
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        }
+
+        if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(5).insert(2).insert(10).insert(1);
+
+console.log(tree);
