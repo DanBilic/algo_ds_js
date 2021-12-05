010c6c356eec41399fd893279cbc248387c25983
DanBilic <bilic90@gmx.de>
2021-12-05 16:27:35 +0100

    [array] trapping rainwater optimal solution

--- concept_algo/trapping_rainwaters(hard_array).js
+++ concept_algo/trapping_rainwaters(hard_array).js
@@ -26,3 +26,36 @@ const getTrappedWater = (heights) => {
 
 console.log("input array: ", heights);
 console.log("result: ", getTrappedWater(heights));
+
+console.log("TRAPPING RAINWATER --> optimal solution:");
+
+const trappingRainwaterOptimal = (heights) => {
+  let left = 0;
+  let right = heights.length - 1;
+  let totalWater = 0;
+  let maxLeft = 0;
+  let maxRight = 0;
+
+  while (left < right) {
+    if (heights[left] <= heights[right]) {
+      if (heights[left] >= maxLeft) {
+        maxLeft = heights[left];
+      } else {
+        totalWater += maxLeft - heights[left];
+      }
+      left++;
+    } else {
+      if (heights[right] >= maxRight) {
+        maxRight = heights[right];
+      } else {
+        totalWater += maxRight - heights[right];
+      }
+      right--;
+    }
+  }
+
+  return totalWater;
+};
+
+console.log("input array: ", heights);
+console.log("optimal result: ", trappingRainwaterOptimal(heights));
