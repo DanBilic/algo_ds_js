82edc3b4f22055fc1ecbb20cd557cd8c6205555e
DanBilic <bilic90@gmx.de>
2021-12-19 18:43:14 +0100

    [bst] right side view

--- concept_algo/runBstQuestions.js
+++ concept_algo/runBstQuestions.js
@@ -1,3 +1,5 @@
 require("./trees/questions/dfs_max_depth");
 console.log("\n ------------ new question -------------");
 require("./trees/questions/bfs_level_order");
+console.log("\n ------------ new question -------------");
+require("./trees/questions/bst_right_side_view");
---
+++ concept_algo/trees/questions/bst_right_side_view.js
@@ -0,0 +1,99 @@
+// LEETCODE 104. Maximum Depth of Binary Tree
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert = (value) => {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  };
+}
+
+const traverse = (node) => {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+};
+
+const tree = new BinarySearchTree();
+tree.insert(5);
+tree.insert(2);
+tree.insert(6);
+tree.insert(7);
+tree.insert(8);
+tree.insert(9);
+
+const bfs = (root) => {
+  const list = [];
+  const queue = [root];
+
+  while (queue.length) {
+    const levelCount = queue.length;
+    let currentCount = 0;
+    const subList = [];
+
+    while (currentCount < levelCount) {
+      const currentNode = queue.shift();
+      subList.push(currentNode.value);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+      currentCount++;
+    }
+    list.push(subList);
+  }
+  return list;
+};
+
+var rightSideView = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  const result = bfs(root).map((ele) => {
+    return ele[ele.length - 1];
+  });
+  return result;
+};
+
+console.log(
+  "binary search tree in JSON format: \n",
+  JSON.stringify(traverse(tree.root))
+);
+
+console.log("level order: ", bfs(tree.root));
+console.log("right side view: ", rightSideView(tree.root));
