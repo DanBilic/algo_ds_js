a869c6a870bf5cebbc5ddf74d154efbeeec3d0ea
DanBilic <bilic90@gmx.de>
2021-12-19 10:33:56 +0100

    [bst] search

--- concept_algo/runBst.js
+++ concept_algo/runBst.js
@@ -1,3 +1,4 @@
-require("./trees/binary_searc_tree");
+require("./trees/binary_search_tree");
 require("./trees/bfs");
 require("./trees/dfs");
+require("./trees/binary_search");
---
+++ concept_algo/trees/binary_search.js
@@ -0,0 +1,26 @@
+const binarySearch = (array, target) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const foundValue = array[mid];
+    if (foundValue === target) {
+      // return index
+      return mid;
+    } else if (foundValue < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log("Binary search: \n");
+console.log("input array: ", sortedArray);
+console.log("target: 3");
+console.log("result: ", binarySearch(sortedArray, 3));
