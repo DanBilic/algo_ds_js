c95d857c5682421afec62e0e0c5d9f66ab63eded
DanBilic <bilic90@gmx.de>
2022-01-24 09:31:58 +0100

    [by_topic][linked_list] squares of sorted array optimized verion

--- by_topic/array/squares_of_sorted_arr_#977_(easy).js
+++ by_topic/array/squares_of_sorted_arr_#977_(easy).js
@@ -14,3 +14,36 @@ var sortedSquares = function (nums) {
 
   return nums;
 };
+
+// optimized version
+// Time: O(n)
+// Space: O(n)
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  const resultArr = new Array(nums.length).fill(0);
+
+  let left = 0;
+  let right = nums.length - 1;
+  let resultIdx = nums.length - 1;
+
+  while (left <= right) {
+    const leftVal = Math.pow(nums[left], 2);
+    const rightVal = Math.pow(nums[right], 2);
+
+    if (rightVal >= leftVal) {
+      resultArr[resultIdx] = rightVal;
+      right--;
+    } else {
+      resultArr[resultIdx] = leftVal;
+      left++;
+    }
+
+    resultIdx--;
+  }
+
+  return resultArr;
+};
