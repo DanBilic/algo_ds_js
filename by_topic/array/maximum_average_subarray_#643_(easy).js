53c4f7d58c3b32f71352bd7deb53ab353776f433
DanBilic <bilic90@gmx.de>
2022-02-28 14:52:06 +0100

    [array] maximum average subarray - clean version

--- by_topic/array/maximum_average_subarray_#643_(easy).js
+++ by_topic/array/maximum_average_subarray_#643_(easy).js
@@ -32,3 +32,29 @@ var findMaxAverage = function (nums, k) {
 
   return max / k;
 };
+
+// clean version with same complexity:
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let max = -Infinity;
+  let soFar = 0;
+  let windowStart = 0;
+
+  for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+    soFar += nums[windowEnd];
+
+    if (windowEnd - windowStart === k - 1) {
+      let avg = soFar / k;
+      max = Math.max(avg, max);
+      soFar -= nums[windowStart];
+      windowStart++;
+    }
+  }
+
+  return max;
+};
