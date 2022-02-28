934558248701f50d364269e29113168739857d05
DanBilic <bilic90@gmx.de>
2022-02-28 18:29:12 +0100

    [linked-list] palindrome linked list- optimised version with O(1) space

--- by_topic/linked_list/palindrome_linked_list_#234_(easy).js
+++ by_topic/linked_list/palindrome_linked_list_#234_(easy).js
@@ -1,4 +1,6 @@
 // Brute force
+// Time: O(n)
+// Space: O(n)
 
 /**
  * Definition for singly-linked list.
@@ -30,4 +32,59 @@ var isPalindrome = function (head) {
   } else {
     return false;
   }
+};
+
+// Optimised version:
+// reverse the second half of the linked list
+// Time: O(n)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+const reverseList = (head) => {
+  let cur = head;
+  let prev = null;
+  let next = null;
+
+  while (cur) {
+    next = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
+  }
+
+  return prev;
+};
+
+var isPalindrome = function (head) {
+  let slow = head;
+  let fast = head;
+
+  // fast.next cuz we use fast.next.next and that would return an error if fast.next is null
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  let mid = reverseList(slow);
+  let pointer = head;
+  while (mid) {
+    if (mid.val !== pointer.val) {
+      return false;
+    }
+    mid = mid.next;
+    pointer = pointer.next;
+  }
+
+  return true;
 };
