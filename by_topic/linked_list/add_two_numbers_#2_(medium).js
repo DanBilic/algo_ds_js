b2b00c102f6905e8fbdcff5bc86784a08491d64d
DanBilic <bilic90@gmx.de>
2022-03-01 10:54:27 +0100

    [linked-list] add two numbers - clean version

--- by_topic/linked_list/add_two_numbers_#2_(medium).js
+++ by_topic/linked_list/add_two_numbers_#2_(medium).js
@@ -40,3 +40,39 @@ var addTwoNumbers = function (l1, l2) {
 
   return dummyHead.next;
 };
+
+// Clean version:
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(-1);
+  let result = dummy;
+  let carry = 0;
+
+  while (l1 || l2 || carry) {
+    let l1Val = l1 ? l1.val : 0;
+    let l2Val = l2 ? l2.val : 0;
+    let sum = l1Val + l2Val + carry;
+    let nextVal = sum % 10;
+
+    result.next = new ListNode(nextVal);
+    result = result.next;
+
+    carry = Math.floor(sum / 10);
+
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+
+  return dummy.next;
+};
